de46f1cc045e99bbb40f30b5466798f2b1a7f1f4
Iuri <iuri.menin@softfocus.com.br>
2016-12-02 12:54:30 -0200

    Getting movies from "theMovieDB"

--- app/src/main/java/io/github/iurimenin/popularmovies/MovieFragment.java
+++ app/src/main/java/io/github/iurimenin/popularmovies/MovieFragment.java
@@ -1,9 +1,12 @@
 package io.github.iurimenin.popularmovies;
 
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +15,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -51,6 +71,12 @@ public class MovieFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        updateMovies();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 
         inflater.inflate(R.menu.moviefragment, menu);
@@ -62,23 +88,111 @@ public class MovieFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    public class ListMoviesTask extends AsyncTask<String, Void, String[]> {
+    private void updateMovies () {
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortBy = prefs.getString(getString(R.string.pref_sort_key),
+                getString(R.string.pref_sort_popular));
+
+        ListMoviesTask fetchWeatherTask = new ListMoviesTask();
+        fetchWeatherTask.execute(sortBy);
+    }
+
+    public class ListMoviesTask extends AsyncTask<String, Void, ArrayList<MovieVO>> {
 
         private final String LOG_TAG = ListMoviesTask.class.getSimpleName();
+        private String json;
 
         @Override
-        protected String[] doInBackground(String... params) {
+        protected ArrayList<MovieVO> doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            try {
+
+                StringBuilder uri = new StringBuilder();
+                uri.append(Utils.MOVIES_URL);
+                uri.append(Utils.SORT_BY);
+                uri.append(params[0]);
+                uri.append(Utils.ASC);
+                uri.append(Utils.API_KEY);
+                uri.append("67fccec8dbc33668cc830348ade95787");
+
+                URL url = new URL(uri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream != null) {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line + "\n");
+                    }
+
+                    if (buffer.length() == 0) {
+                        return null;
+                    }
+                    json = buffer.toString();
+                }
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMoviesDataFromJson(json);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
 
             return null;
         }
 
+        private ArrayList<MovieVO> getMoviesDataFromJson(String json) throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String TITLE = "original_title";
+            final String RELEASE_DATE= "release_date";
+            final String POSTER = "poster_path";
+            final String VOTE_AVARAGE = "vote_average";
+            final String SYNOPSIS = "overview";
+            final String RESULT = "results";
+
+            JSONObject moviesJson = new JSONObject(json);
+            JSONArray moviesArray = moviesJson.getJSONArray(RESULT);
+
+            Type typeMovies = new TypeToken<List<MovieVO>>() {}.getType();
+            ArrayList<MovieVO> moviesVO = new Gson().fromJson(moviesArray.toString(), typeMovies);
+
+            return moviesVO;
+        }
+
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(ArrayList<MovieVO> result) {
 
             if (result == null)
                 return;
-            mForecastAdapter.clear();
-            mForecastAdapter.addAll(result);
+           /* mForecastAdapter.clear();
+            mForecastAdapter.addAll(result); TODO*/
         }
     }
 }
--- app/src/main/java/io/github/iurimenin/popularmovies/Utils.java
+++ app/src/main/java/io/github/iurimenin/popularmovies/Utils.java
@@ -7,11 +7,12 @@ package io.github.iurimenin.popularmovies;
 public class Utils {
 
     public static final String IMAGES_URL = "https://image.tmdb.org/t/p/w500/";
-    public static final String MOVIES_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=";
-    public static final String LANGUAGE = "&language=pt-BR";
-    public static final String SORTY_BY_VOTE = "vote_average.asc";
-    public static final String SORTY_BY_POPULARITY = "popularity.asc";
-    public static final String API_KEY = "&api_key=67fccec8dbc33668cc830348ade95787";
+    public static final String MOVIES_URL = "https://api.themoviedb.org/3/discover/movie?";
+    public static final String SORT_BY = "sort_by=";
+    public static final String SORTY_BY_VOTE = "vote_average";
+    public static final String SORTY_BY_POPULARITY = "popularity";
+    public static final String ASC = ".asc";
+    public static final String API_KEY = "&api_key=";
 
     //https://api.themoviedb.org/3/discover/movie?sort_by=popularity.asc&api_key=67fccec8dbc33668cc830348ade95787
     //http://api.themoviedb.org/3/discover/movie?sort_by=&api_key=67fccec8dbc33668cc830348ade95787
